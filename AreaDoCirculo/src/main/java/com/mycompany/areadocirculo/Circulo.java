63002d9e78d46538b75975d5432e34986c78c705
Ana Gabriela Napoleão Silva <135245621+gabriielanapoleao@users.noreply.github.com>
2023-08-17 08:21:18 -0300

    Add files via upload

---
+++ AreaDoCirculo/src/main/java/com/mycompany/areadocirculo/Circulo.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.areadocirculo;
+
+public class Circulo {
+    
+    private double areaRaio;
+    private double pi=3.14;
+    private double raio;
+    
+    public double  getCalcular (double pi,double raio){
+        return this.raio = pi*(raio*raio);
+    }
+    
+    public double  getmostrarArea(){
+        return this.raio = this.areaRaio;
+    }
+
+}
---
+++ AreaDoCirculo/src/main/java/com/mycompany/areadocirculo/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.areadocirculo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner= new Scanner (System.in);
+        
+        
+        double areaRaio;
+        
+        System.out.println("Informe a área do raio: ");
+        areaRaio = scanner.nextDouble();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
